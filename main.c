b08e3283c228976f244cbea59394864716e1c7f1
matubu <mberger-@student.42nice.fr>
2022-05-25 22:32:04 +0200

    start to send

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+
+#define BUF_SIZE 1024
 
 int		verbose = 0;
 int		help = 0;
@@ -31,7 +36,7 @@ int		error_resolution(const char *host)
 
 void	statistics(void)
 {
-	puts("");
+	printf("\n");
 	printf("--- %s ft_ping statistics ---\n", host);
 	printf("%d packets transmitted, %d packets received, %.1f%% packet loss\n",
 		packets_count,
@@ -41,37 +46,45 @@ void	statistics(void)
 	exit(0);
 }
 
+int	sock;
+
 void	ping(void)
 {
 	int		icmp_seq = packets_count;
 	int		ttl = 0;
 	float	time = 0;
+
+	errno = 0;
+	ssize_t	sent = sendto(sock, "Hello world", 11, 0, addr->ai_addr, addr->ai_addrlen);
+	printf("sent %ld\n", sent);
+	
+	perror("sendto");
+
+	char			buf[BUF_SIZE];
+	struct iovec	iov = {buf, BUF_SIZE};
+	struct msghdr	msg = {
+		.msg_name = addr->ai_addr,
+		.msg_namelen = addr->ai_addrlen,
+		.msg_iov = &iov,
+		.msg_iovlen = 1,
+		.msg_control = NULL,
+		.msg_controllen = 0,
+		.msg_flags = 0
+	};
+	errno = 0;
+	ssize_t	len = recvmsg(sock, &msg, 0);
+	// ssize_t	len = 0;
+
+	perror("recvmsg");
+	printf("msg received\n");
+	if (len > 0)
+		printf("write %ld\n", write(1, buf, len));
+
 	++packets_count;
-	alarm(5);
-
-	int	sock = socket(addr->ai_family, SOCK_RAW, IPPROTO_ICMP);
-	if (sock < 0)
-	{
-		printf("%d\n", AF_INET == addr->ai_family);
-		puts("ping: socket: Operation not permitted");
-		return ;
-	}
-	else
-		printf("yes");
-	// if (bind(sock, addr->ai_addr, sizeof(*addr->ai_addr)) < 0)
-	// {
-	// 	printf("bind failed\n");
-	// 	return ;
-	// }
-	// printf("test\n");
-	// sendto(sock, "Hello world", 11, 0, addr->ai_addr, sizeof(*addr->ai_addr));
-
-	// struct msghdr	msg;
-	// ssize_t	len = recvmsg(sock, &msg, 0);
-	ssize_t	len = 0;
-
 	//++packets_received;
 	printf("%zd bytes from %s: icmp_seq=%d ttl=%d time=%.3f\n", len, ip, icmp_seq, ttl, time);
+
+	alarm(1);
 }
 
 int		main(int ac, const char **av)
@@ -95,13 +108,19 @@ int		main(int ac, const char **av)
 	if (!host || help)
 		return (show_help());
 
-	if (getaddrinfo(host, "0", 0, &addr) != 0)
+	if (getaddrinfo(host, "0", 0, &addr) < 0)
 		return (error_resolution(host));
 
-	inet_ntop(addr->ai_family, &((struct sockaddr_in *)addr->ai_addr)->sin_addr, ip, INET6_ADDRSTRLEN);
+	sock = socket(addr->ai_family, SOCK_RAW, IPPROTO_ICMP);
+	if (sock < 0)
+	{
+		printf("ping: socket: Operation not permitted\n");
+		exit(1);
+	}
 
+	inet_ntop(addr->ai_family, &((struct sockaddr_in *)addr->ai_addr)->sin_addr, ip, INET6_ADDRSTRLEN);
 	printf("PING %s (%s): 56 data bytes\n", host, ip);
-	
+
 	signal(SIGALRM, (void (*)())ping);
 	ping();
 
